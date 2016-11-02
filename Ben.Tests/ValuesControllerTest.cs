795585a702ecd781578501dbc06165fc8e82cda8
Chris Bayles <cbayles@defisolutions.com>
2016-11-02 15:42:11 -0500

    created blank web api project, added values api

---
+++ Ben.Tests/ValuesControllerTest.cs
@@ -0,0 +1,103 @@
+﻿using System.Linq;
+using Ben.Controllers;
+using NUnit.Framework;
+
+namespace Ben.Tests
+{
+    [TestFixture]
+    public class ValuesControllerTest
+    {
+        [Test]
+        public void Get()
+        {
+            // Arrange
+            var controller = new ValuesController();
+            //todo: ???
+
+            // Act
+            var result = controller.Get();
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(2, result.Count());
+            Assert.AreEqual("", result.ElementAt(0));
+            Assert.AreEqual("", result.ElementAt(1));
+        }
+
+        [Test]
+        public void GetById()
+        {
+            // Arrange
+            var controller = new ValuesController();
+            //todo: ???
+
+            // Act
+            var result = controller.Get(1).ToString();
+
+            // Assert
+            Assert.AreEqual("Ben has done it", result);
+            Assert.Fail("fill in the blanks buddy");
+        }
+
+        [Test]
+        public void Post()
+        {
+            // Arrange
+            ValuesController controller = new ValuesController();
+
+            // Act
+            var result = controller.Post("value");
+
+            // Assert
+            //            Assert.IsInstanceOfType(result, typeof(CreatedNegotiatedContentResult<string>));
+            //            Assert.AreEqual("?????", ((CreatedNegotiatedContentResult<string>)result).Location.ToString());
+            Assert.Fail("fill in the blanks buddy");
+        }
+
+        [Test]
+        public void Put()
+        {
+            // Arrange
+            ValuesController controller = new ValuesController();
+            // todo: add item to apply the PUT to.
+
+            // Act
+            // todo: var result = controller.Put(???, "value");
+
+            // Assert
+            // Assert.IsInstanceOfType(result, typeof(???));
+            Assert.Fail("fill in the blanks buddy");
+        }
+
+        [Test]
+        public void Put_DoesNotExist()
+        {
+            // Arrange
+            ValuesController controller = new ValuesController();
+
+            // Act
+            var result = controller.Put(999, "value");
+
+            // Assert
+            // todo: what should the result be if id 5 does not exist? 
+            // Assert.IsInstanceOfType(result, typeof(???));
+            Assert.Fail("fill in the blanks buddy");
+        }
+
+        [Test]
+        public void Delete()
+        {
+            // Arrange
+            ValuesController controller = new ValuesController();
+            // todo: add item to apply the DELETE to.
+
+            // Act
+            // todo: controller.Delete(???);
+
+            // Assert
+            // todo: what response code do you expect?
+            // todo: how can you double check the item was deleted?
+            Assert.Fail("fill in the blanks buddy");
+        }
+    }
+}
---
+++ Ben/Controllers/ValuesController.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace Ben.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // static is used so this field lives for the life of the app, rather than per request.
+        private static readonly SortedDictionary<int, string> ValuesDb = new SortedDictionary<int, string>();
+
+        // GET api/values
+        public IEnumerable<dynamic> Get()
+        {
+            return ValuesDb
+                .Select(v => new { id = v.Key, value = v.Value })
+                .ToList();
+        }
+
+        // GET api/values/5
+        public IHttpActionResult Get(int id)
+        {
+            if (!ValuesDb.ContainsKey(id))
+            {
+                return NotFound();
+            }
+            return Ok(ValuesDb[id]);
+        }
+
+        // POST api/values
+        public IHttpActionResult Post([FromBody]string value)
+        {
+            var nextId = NextId();
+
+            ValuesDb.Add(nextId, value);
+
+            return Created("api/values/" + nextId, value);
+        }
+
+        private static int NextId()
+        {
+            if (ValuesDb.Keys.Count == 0)
+            {
+                return 1;
+            }
+
+            return ValuesDb.Keys.Max() + 1;
+        }
+
+        // PUT api/values/5
+        public IHttpActionResult Put(int id, [FromBody]string value)
+        {
+            if (!ValuesDb.ContainsKey(id))
+            {
+                return NotFound();
+            }
+
+            ValuesDb[id] = value;
+
+            return Ok();
+        }
+
+        // DELETE api/values/5
+        public IHttpActionResult Delete(int id)
+        {
+            if (!ValuesDb.ContainsKey(id))
+            {
+                return NotFound();
+            }
+
+            ValuesDb.Remove(id);
+
+            return Ok();
+        }
+    }
+}
